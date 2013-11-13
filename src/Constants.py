617ea4f684d7333d28895be0e8192430cca59580
ikoichi <ikoichi2112@gmail.com>
2013-11-13 08:53:52 -0800

    user.ini renamed to user.json

--- src/Constants.py
+++ src/Constants.py
@@ -6,7 +6,7 @@ AUTHOR              = "Luca Restagno <luca.restagno@gmail.com>"
 APP_TITLE           = "Markdown Editor"
 EMPTY_TAB_TITLE     = "Untitled"
 EDIT_FONT           = "Consolas"
-CONFIG_FILE         = "user.ini"
+CONFIG_FILE         = "user.json"
 HELP_FILE           = "resources/help.md"
 LICENSE             = "MIT"
         
